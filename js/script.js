6c362dd1dcd5294a37b09bfdde81f6559fb38a5e
RDemian <rdemian@github.com>
2017-12-07 15:24:38 +0500

    ещё исправления

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ var feedbackForm = document.querySelector('.feedback');
 var feedbackBtn = document.querySelector('.feedback-btn');
 var feedbackClose = document.querySelector('.close-feedback');
 
+
 feedbackBtn.addEventListener('click', function () {
   feedbackForm.classList.add('feedback-show');
+
 });
 
 feedbackClose.addEventListener('click', function () {
