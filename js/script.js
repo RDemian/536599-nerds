dd1089f1f2ebe4e647d6f689a357ce5147c0a506
RDemian <rdemian@github.com>
2017-12-12 11:30:22 +0500

    Исправление замечаний 2

--- js/script.js
+++ js/script.js
@@ -12,3 +12,36 @@ feedbackBtn.addEventListener('click', function (evt) {
 feedbackClose.addEventListener('click', function () {
   feedbackForm.classList.remove('feedback-show');
 });
+
+ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+            center: [59.93863106417265,30.3230545],
+            zoom: 16
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+
+        // Создаём макет содержимого.
+       MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+            '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+        ),
+
+        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+            hintContent: 'Собственный значок метки',
+            balloonContent: 'Это красивая метка'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: 'img/map-marker.png',
+            // Размеры метки.
+            iconImageSize: [231, 190],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-50, -195]
+        });
+
+    myMap.geoObjects
+        .add(myPlacemark);
+});
